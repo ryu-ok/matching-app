57689824e50d803a87473420dc94e3165a2b143a
ryu-ok <nfc.63.lddal@gmail.com>
2020-04-07 19:55:38 +0900

    Update carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,17 +1,17 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws' 
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'], 
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } 
+  end
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'matchingapp007'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/matchingapp007'
+  # 日本語ファイル名の設定
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
 end
